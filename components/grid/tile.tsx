a3428481629e5a4f91d7715b67f24254dd09504b
wav_4m <ceisenbach@gmail.com>
2026-07-24 22:02:45 -0500

    carousel outline

--- components/grid/tile.tsx
+++ components/grid/tile.tsx
@@ -23,7 +23,7 @@ export function GridTileImage({
         "group flex h-full w-full items-center justify-center overflow-hidden rounded-lg border bg-white hover:border-blue-600 dark:bg-black",
         {
           relative: label,
-          "border-2 border-blue-600": active,
+          "border-2 border-red-600": active,
           "border-neutral-200 dark:border-neutral-800": !active,
         },
       )}
