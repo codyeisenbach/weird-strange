c5cc4e374d855d3f62094656fb804db6e000dc0a
J Cody Eisenbach <ceisenbach@gmail.com>
2026-08-11 21:43:00 -0500

    added gtm web container

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,7 +32,30 @@ export default async function RootLayout({
 
   return (
     <html lang="en" className={GeistSans.variable}>
+      <head>
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-N99WWR99');`,
+          }}
+        />
+        {/* End Google Tag Manager */}
+      </head>
       <body className="bg-ws-cream flex justify-center flex-col w-full text-ws-charcoal selection:bg-teal-300 dark:selection:bg-pink-500 dark:selection:text-white">
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-N99WWR99"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         <CartProvider cartPromise={cart}>
           <SaleBanner />
           <Navbar />
